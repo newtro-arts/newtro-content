ffcaa3bfeb5b7e923db3a7514e5d1de26a573ab7
Sweets Sweetman <sweetmantech@gmail.com>
2024-10-10 21:34:59 -0300

    I remove unused referenceId.

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,23 +1,25 @@
-import { createChatLLMService, StreamResponseSchema } from '@/lib/server/chat-llm.service';
-import { enhanceRouteHandler } from '@/packages/next/src/routes';
+import {
+  createChatLLMService,
+  StreamResponseSchema,
+} from "@/lib/server/chat-llm.service";
+import { enhanceRouteHandler } from "@/packages/next/src/routes";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 export const POST = enhanceRouteHandler(
   async ({ body }) => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const service = createChatLLMService();
-    const referenceId = "if1Fg9bo"; // hard coded for demo
 
-    try {    
-      return await service.streamResponse(body, referenceId);
+    try {
+      return await service.streamResponse(body);
     } catch (error) {
       console.error(error);
-      const message = error instanceof Error ? error.message : 'Unknown error';
+      const message = error instanceof Error ? error.message : "Unknown error";
       return new Response(message, { status: 500 });
     }
   },
   {
     schema: StreamResponseSchema,
-  },
+  }
 );
